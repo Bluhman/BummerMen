4cb0eb8edfb11a33b3bd1150efae6e7af728a700
Micah Fujiwara <3156272+Bluhman@users.noreply.github.com>
2018-04-19 17:19:10 -0500

    Briefing
    Play button now leads to a briefing screen.
    Can now use start button on splash screen to start game (instead of needing to press A)

---
+++ Assets/Scripts/NonNetworkScripts/StartButton.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using InControl;
+
+/// <summary>
+/// Lets the user use the Start button to move to a new scene.
+/// </summary>
+
+public class StartButton : MonoBehaviour {
+
+    public int newScene;
+    
+	
+	// Update is called once per frame
+	void Update () {
+		if (InputManager.ActiveDevice.MenuWasPressed)
+        {
+            print("LOAD IT!");
+            GameController.instance.LoadNewScene(newScene);
+        }
+	}
+}
---
+++ Assets/Scripts/NonNetworkScripts/TextIntroScroll.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TextIntroScroll : MonoBehaviour {
+
+    Vector2 startPos;
+    public Vector2 endPos;
+    public float scrollDuration;
+    //float scrollSpeed;
+    float scrollTimer;
+    public int nextScene;
+
+	// Use this for initialization
+	void Start () {
+        startPos = transform.position;
+        scrollTimer = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        scrollTimer += Time.deltaTime;
+        transform.position = Vector2.Lerp(startPos, endPos, scrollTimer / scrollDuration);
+
+        if (scrollTimer >= scrollDuration)
+        {
+            GameController.instance.LoadNewScene(nextScene);
+        }
+	}
+}
