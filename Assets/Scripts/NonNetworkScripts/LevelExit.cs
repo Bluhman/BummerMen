50fda9898bf0e19db33fcb006dbf599d6f4facef
Bluhman <3156272+Bluhman@users.noreply.github.com>
2018-04-27 13:46:10 -0500

    LEVEL EXIT!

---
+++ Assets/Scripts/NonNetworkScripts/LevelExit.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelExit : MonoBehaviour {
+
+    public PlayerHUDControllerSP playerHud;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (playerHud.howToWin == PlayerHUDControllerSP.winCondition.ReachExit)
+        {
+            playerHud.Win();
+            Destroy(gameObject);
+        }
+    }
+}
